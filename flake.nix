ab56148629e4ca821d123d31af5eba5f6952f079
Alexey Lebedeff <binarin@binarin.info>
2025-12-27 19:44:39 +0100

    Chapter 01

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Development environment for command-line-rust";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        devShells.default = pkgs.mkShell {
+          name = "command-line-rust-dev-shell";
+          meta.description = "Command Line Rust development environment";
+          packages = with pkgs; [
+            cargo
+            rustc
+            rust-analyzer
+            clippy
+            rustfmt
+
+            # rustic - rust docs in org-mode
+            pandoc
+            # cargo-makedocs
+            fd
+          ];
+        };
+      };
+    };
+}
