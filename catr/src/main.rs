0490614bbdefe9ec63db6017bcbc834da7652e29
Alexey Lebedeff <binarin@binarin.info>
2025-12-29 19:52:17 +0100

    Chapter 03 - convert args parsing to use clap derive

--- catr/src/main.rs
+++ catr/src/main.rs
@@ -1,51 +1,23 @@
-use clap::{Arg, ArgAction, Command};
+use clap::Parser;
 
-#[derive(Debug)]
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+/// Rust version of cat ‘cat’
 struct Args {
+    /// Input file(s)
+    #[arg(value_name = "FILE", default_value = "-")]
     files: Vec<String>,
+
+    /// Number lines
+    #[arg(short('n'), long("number"), conflicts_with("number_nonblank_lines"))]
     number_lines: bool,
+
+    /// Number non-blank lines
+    #[arg(short('b'), long("number-nonblank"))]
     number_nonblank_lines: bool,
 }
 
-pub fn get_args() -> Args {
-    let matches = Command::new("catr")
-        .version("0.1.0")
-        .author("Alexey Lebedeff <learning-rust@binarin.info>")
-        .about("Rust cat")
-        .arg(Arg::new("number_lines")
-             .short('n')
-             .long("number")
-             .help("number all output lines")
-             .action(ArgAction::SetTrue)
-        )
-        .arg(Arg::new("number_nonblank_lines")
-             .short('b')
-             .long("number-nonblank")
-             .help("number nonempty output lines, overrides -n")
-             .action(ArgAction::SetTrue)
-             .conflicts_with("number_lines")
-        )
-        .arg(Arg::new("files")
-             .value_name("FILE")
-             .help("With no FILE, or when FILE is -, read standard input.")
-             .num_args(1..),
-        )
-        .get_matches();
-
-    let files: Vec<String> = matches
-        .get_many("files")
-        .map(|it| it.cloned().collect())
-        .unwrap_or(vec!["-".to_string()]);
-
-    Args{
-        files,
-        number_lines: matches.get_flag("number_lines"),
-        number_nonblank_lines: matches.get_flag("number_nonblank_lines"),
-    }
-}
-
-
 fn main() {
-    let args = get_args();
+    let args = Args::parse();
     dbg!(args);
 }
