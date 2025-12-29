cd3402d5e41aab3ec9cdaa39878c7a787b3a7e4b
Alexey Lebedeff <binarin@binarin.info>
2025-12-29 19:40:42 +0100

    Chapter 03 - update arg parser to the new clap version

--- catr/src/main.rs
+++ catr/src/main.rs
@@ -1,3 +1,5 @@
+use clap::{Arg, ArgAction, Command};
+
 #[derive(Debug)]
 struct Args {
     files: Vec<String>,
@@ -5,6 +7,42 @@ struct Args {
     number_nonblank_lines: bool,
 }
 
+pub fn get_args() -> Args {
+    let matches = Command::new("catr")
+        .version("0.1.0")
+        .author("Alexey Lebedeff <learning-rust@binarin.info>")
+        .about("Rust cat")
+        .arg(Arg::new("number_lines")
+             .short('n')
+             .long("number")
+             .help("number all output lines")
+             .action(ArgAction::SetTrue)
+        )
+        .arg(Arg::new("number_nonblank_lines")
+             .short('b')
+             .long("number-nonblank")
+             .help("number nonempty output lines, overrides -n")
+             .action(ArgAction::SetTrue)
+        )
+        .arg(Arg::new("files")
+             .value_name("FILE")
+             .help("With no FILE, or when FILE is -, read standard input.")
+             .num_args(1..),
+        )
+        .get_matches();
+
+    let files: Vec<String> = matches.get_many("files").unwrap().cloned().collect();
+
+
+    Args{
+        files,
+        number_lines: matches.get_flag("number_lines"),
+        number_nonblank_lines: matches.get_flag("number_nonblank_lines"),
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let args = get_args();
+    dbg!(args);
 }
