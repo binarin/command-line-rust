aa98e1cac699be47dbe8f8af3e93677e92975881
Alexey Lebedeff <binarin@binarin.info>
2025-12-31 11:01:35 +0100

    Chapter 05 - only enable default options when no explicit options are present

--- wcr/src/main.rs
+++ wcr/src/main.rs
@@ -9,15 +9,15 @@ struct Args {
     /// filenames (or ‘-’ for stdin)
     files: Vec<String>,
 
-    #[arg(short, long, default_value_t = true)]
+    #[arg(short, long, default_value_t = false)]
     /// print the newline counts
     lines: bool,
 
-    #[arg(short, long, default_value_t = true)]
+    #[arg(short, long, default_value_t = false)]
     /// print the word count
     words: bool,
 
-    #[arg(short('c'), long, default_value_t = true)]
+    #[arg(short('c'), long, default_value_t = false)]
     /// print the bytes count
     bytes: bool,
 
@@ -27,7 +27,7 @@ struct Args {
 }
 
 fn main() {
-    run(Args::parse()).unwrap_or_else(|err| {
+    run(parse_args()).unwrap_or_else(|err| {
         eprintln!("{err}");
         std::process::exit(1);
     });
@@ -36,4 +36,16 @@ fn main() {
 fn run(args: Args) -> Result<()> {
     dbg!(args);
     Ok(())
+}
+
+fn parse_args() -> Args {
+    let mut args = Args::parse();
+
+    // none of the explicit args is present
+    if !(args.lines || args.words || args.bytes || args.chars) {
+        args.lines = true;
+        args.words = true;
+        args.bytes = true;
+    }
+    args
 }
