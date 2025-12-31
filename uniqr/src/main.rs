1ef7bbd47f1842c082c5ae12b217df2d2b6ad47f
Alexey Lebedeff <binarin@binarin.info>
2025-12-31 20:34:41 +0100

    Chapter 06 - add argument parsing

--- uniqr/src/main.rs
+++ uniqr/src/main.rs
@@ -1,3 +1,33 @@
+use std::{fs::File, io::{self, BufRead, BufReader}};
+
+use anyhow::Result;
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+/// ‘uniq’ in Rust - omit repeated lines
+struct Args {
+    #[arg(value_name("INPUT"), default_value = "-")]
+    in_file: String,
+
+    #[arg(value_name("OUTPUT"))]
+    out_file: Option<String>,
+
+    #[arg(short, long)]
+    /// prefix lines by the number of occurences
+    count: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    run(Args::parse()).unwrap_or_else(|err| {
+        eprintln!("{err}");
+        std::process::exit(1);
+    });
+}
+
+fn run(args: Args) -> Result<()> {
+    dbg!(args);
+    Ok(())
+}
+
 }
