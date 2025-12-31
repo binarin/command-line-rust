6ec454a4c9ba0d20e85692fbbefc5f3f2cd07a04
Alexey Lebedeff <binarin@binarin.info>
2025-12-31 10:44:00 +0100

    Chapter 05 - fix deprecations in tests

--- wcr/tests/cli.rs
+++ wcr/tests/cli.rs
@@ -1,11 +1,10 @@
 use anyhow::Result;
-use assert_cmd::Command;
+use assert_cmd::cargo::cargo_bin_cmd;
 use predicates::prelude::*;
 use pretty_assertions::assert_eq;
 use rand::{distributions::Alphanumeric, Rng};
 use std::fs;
 
-const PRG: &str = "wcr";
 const EMPTY: &str = "tests/inputs/empty.txt";
 const FOX: &str = "tests/inputs/fox.txt";
 const ATLAMAL: &str = "tests/inputs/atlamal.txt";
@@ -28,7 +27,7 @@ fn gen_bad_file() -> String {
 // --------------------------------------------------
 #[test]
 fn dies_chars_and_bytes() -> Result<()> {
-    Command::cargo_bin(PRG)?
+    cargo_bin_cmd!()
         .args(["-m", "-c"])
         .assert()
         .failure()
@@ -41,7 +40,7 @@ fn dies_chars_and_bytes() -> Result<()> {
 // --------------------------------------------------
 fn run(args: &[&str], expected_file: &str) -> Result<()> {
     let expected = fs::read_to_string(expected_file)?;
-    let output = Command::cargo_bin(PRG)?.args(args).output().expect("fail");
+    let output = cargo_bin_cmd!().args(args).output().expect("fail");
     assert!(output.status.success());
 
     let stdout = String::from_utf8(output.stdout).expect("invalid UTF-8");
@@ -55,7 +54,7 @@ fn run(args: &[&str], expected_file: &str) -> Result<()> {
 fn skips_bad_file() -> Result<()> {
     let bad = gen_bad_file();
     let expected = format!("{bad}: .* [(]os error 2[)]");
-    Command::cargo_bin(PRG)?
+    cargo_bin_cmd!()
         .arg(bad)
         .assert()
         .success()
@@ -166,7 +165,7 @@ fn atlamal_stdin() -> Result<()> {
     let expected =
         fs::read_to_string("tests/expected/atlamal.txt.stdin.out")?;
 
-    let output = Command::cargo_bin(PRG)?
+    let output = cargo_bin_cmd!()
         .write_stdin(input)
         .output()
         .expect("fail");
