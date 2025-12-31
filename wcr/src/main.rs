cfa12d72cb0fd8d9568730c00d1d2d60d2cf9f40
Alexey Lebedeff <binarin@binarin.info>
2025-12-31 10:44:16 +0100

    Chapter 05 - implement args parser

---
+++ wcr/src/main.rs
@@ -0,0 +1,36 @@
+use clap::Parser;
+use anyhow::Result;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+/// ‘wc’ in Rust
+struct Args {
+    #[arg(value_name="FILE", default_value="-")]
+    /// filenames (or ‘-’ for stdin)
+    files: Vec<String>,
+
+    #[arg(short, long, default_value_t = true)]
+    /// print the newline counts
+    lines: bool,
+
+    #[arg(short, long, default_value_t = true)]
+    /// print the word count
+    words: bool,
+
+    #[arg(short('c'), long, default_value_t = true)]
+    /// print the bytes count
+    bytes: bool,
+
+    #[arg(short('m'), long, default_value_t = true)]
+    /// print the characters count
+    chars: bool,
+}
+
+fn main() {
+    run(Args::parse()).unwrap_or_else(|err| eprintln!("{err}"))
+}
+
+fn run(args: Args) -> Result<()> {
+    dbg!(args);
+    Ok(())
+}
