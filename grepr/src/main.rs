9082bd865965d3a3be1a7025f14820965f490cf1
Alexey Lebedeff <binarin@binarin.info>
2026-01-18 18:44:37 +0100

    Chapter 09 - Use type-system to distinguish STDIN (instead of special-casing "-")

--- grepr/src/main.rs
+++ grepr/src/main.rs
@@ -1,5 +1,13 @@
-use anyhow::{Result, anyhow};
+use std::fmt::Display;
+
+use anyhow::{anyhow, Result};
 use clap::Parser;
+
+#[derive(Debug, Clone, PartialEq)]
+enum Input {
+    File(String),
+    StdIn,
+}
 
 /// Rust version of ‘grep’
 #[derive(Debug, Parser)]
@@ -9,8 +17,8 @@ struct Args {
     pattern: String,
 
     /// Input files(s)
-    #[arg(default_value = "-", value_name = "FILE")]
-    files: Vec<String>,
+    #[arg(default_value = "-", value_name = "FILE", value_parser = parse_input)]
+    files: Vec<Input>,
 
     /// Case-insensitive
     #[arg(short, long)]
@@ -46,14 +54,22 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn find_files(paths: &[String], recursive: bool) -> Vec<Result<String>> {
-    let mut result = Vec::new();
+fn parse_input(filename: &str) -> Result<Input> {
+    match filename {
+        "-" => Ok(Input::StdIn),
+        _ => Ok(Input::File(filename.to_string())),
+    }
+}
 
-    for path in paths {
-        if path == "-" {
-            result.push(Ok("-".to_string()));
+fn find_files(paths: &[Input], recursive: bool) -> Vec<Result<Input>> {
+    let mut result: Vec<Result<Input>> = Vec::new();
+
+    for input in paths {
+        let Input::File(path) = input else {
+            result.push(Ok(input.clone()));
             continue;
-        }
+        };
+
         if !recursive {
             let single_res = std::fs::metadata(path)
                 .map_err(From::from)
@@ -61,7 +77,7 @@ fn find_files(paths: &[String], recursive: bool) -> Vec<Result<String>> {
                     if metadata.is_dir() {
                         Err(anyhow!("{path} is a directory"))
                     } else {
-                        Ok(path.to_string())
+                        Ok(input.clone())
                     }
                 });
             result.push(single_res);
@@ -78,7 +94,7 @@ fn find_files(paths: &[String], recursive: bool) -> Vec<Result<String>> {
                             None => result.push(Err(anyhow!(
                                 "Failed to convert dent path '{dent:?}' to string"
                             ))),
-                            Some(s) => result.push(Ok(s.to_string())),
+                            Some(s) => result.push(Ok(Input::File(s.to_string()))),
                         }
                     }
                 }
@@ -89,33 +105,53 @@ fn find_files(paths: &[String], recursive: bool) -> Vec<Result<String>> {
     result
 }
 
+impl Display for Input {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Input::StdIn => f.write_str("<STDIN>"),
+            Input::File(file) => file.fmt(f),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use super::find_files;
-    use rand::{Rng, distributions::Alphanumeric};
+    use super::*;
+    use rand::{distributions::Alphanumeric, Rng};
     #[test]
     fn test_find_files() {
         // "-" is a special case, we shouldn’t check whether it exists or not
-        let files = find_files(&["-".to_string()], false);
+        let files = find_files(&[Input::StdIn], false);
         assert_eq!(files.len(), 1);
-        assert_eq!(files[0].as_ref().unwrap(), "-");
+        assert_eq!(*files[0].as_ref().unwrap(), Input::StdIn);
 
         // Verify that the function finds a file known to exist
-        let files = find_files(&["./tests/inputs/fox.txt".to_string()], false);
+        let files = find_files(&[Input::File("./tests/inputs/fox.txt".to_string())], false);
         assert_eq!(files.len(), 1);
-        assert_eq!(files[0].as_ref().unwrap(), "./tests/inputs/fox.txt");
+        assert_eq!(
+            *files[0].as_ref().unwrap(),
+            Input::File("./tests/inputs/fox.txt".to_string())
+        );
 
         // The function should reject a directory without the recursive option
-        let files = find_files(&["./tests/inputs".to_string()], false);
+        let files = find_files(&[Input::File("./tests/inputs".to_string())], false);
         assert_eq!(files.len(), 1);
         if let Err(e) = &files[0] {
             assert_eq!(e.to_string(), "./tests/inputs is a directory");
         }
+        // XXX else check
+
         // Verify the function recurses to find four files in the directory
-        let res = find_files(&["./tests/inputs".to_string()], true);
+        let res = find_files(&[Input::File("./tests/inputs".to_string())], true);
         let mut files: Vec<String> = res
             .iter()
-            .map(|r| r.as_ref().unwrap().replace("\\", "/"))
+            .map(|r| {
+                let Ok(Input::File(f)) = r else {
+                    panic!("No {r:?} expected");
+                };
+                f
+            })
+            .map(|r| r.replace("\\", "/"))
             .collect();
         files.sort();
         assert_eq!(files.len(), 4);
@@ -128,6 +164,7 @@ mod tests {
                 "./tests/inputs/nobody.txt",
             ]
         );
+
         // Generate a random string to represent a nonexistent file
         let bad: String = rand::thread_rng()
             .sample_iter(&Alphanumeric)
@@ -135,7 +172,7 @@ mod tests {
             .map(char::from)
             .collect();
         // Verify that the function returns the bad file as an error
-        let files = find_files(&[bad], false);
+        let files = find_files(&[Input::File(bad)], false);
         assert_eq!(files.len(), 1);
         assert!(files[0].is_err());
     }
