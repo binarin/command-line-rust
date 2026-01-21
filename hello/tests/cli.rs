7a13201236025a41366f283e6a1946d35e6f819e
Alexey Lebedeff <binarin@binarin.info>
2026-01-21 10:05:46 +0100

    Fix deprecation warnings in `hello`

--- hello/tests/cli.rs
+++ hello/tests/cli.rs
@@ -1,19 +1,19 @@
-use assert_cmd::Command;
+use assert_cmd::cargo::cargo_bin_cmd;
 
 #[test]
 fn runs() {
-    let mut cmd = Command::cargo_bin("hello").unwrap();
-   cmd.assert().success().stdout("Hello, world!\n");
+    cargo_bin_cmd!()
+        .assert()
+        .success()
+        .stdout("Hello, world!\n");
 }
 
 #[test]
 fn true_ok() {
-    let mut cmd = Command::cargo_bin("true").unwrap();
-    cmd.assert().success();
+    cargo_bin_cmd!("true").assert().success();
 }
 
 #[test]
 fn false_not_ok() {
-    let mut cmd = Command::cargo_bin("false").unwrap();
-    cmd.assert().failure();
+    cargo_bin_cmd!("false").assert().failure();
 }
