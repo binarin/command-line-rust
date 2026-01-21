30076b482c11c10eecbb4db7cd31a54aed4140f0
Alexey Lebedeff <binarin@binarin.info>
2026-01-21 09:29:02 +0100

    Chapter 10 - convert tests to use helper macros

--- commr/tests/cli.rs
+++ commr/tests/cli.rs
@@ -88,16 +88,6 @@ macro_rules! run {
     }};
 }
 
-fn run(args: &[&str], expected_file: &str) -> Result<()> {
-    let expected = fs::read_to_string(expected_file)?;
-    let output = cargo_bin_cmd!().args(args).output().expect("fail");
-    assert!(output.status.success());
-
-    let stdout = String::from_utf8(output.stdout).expect("invalid UTF-8");
-    assert_eq!(stdout, expected);
-    Ok(())
-}
-
 // --------------------------------------------------
 macro_rules! run_stdin {
     ($input_file:expr , $expected_file:expr , $($args:expr),* $(,)? ) => {{
@@ -120,21 +110,6 @@ macro_rules! run_stdin {
     }};
 }
 
-fn run_stdin(args: &[&str], input_file: &str, expected_file: &str) -> Result<()> {
-    let input = fs::read_to_string(input_file)?;
-    let expected = fs::read_to_string(expected_file)?;
-    let output = cargo_bin_cmd!()
-        .args(args)
-        .write_stdin(input)
-        .output()
-        .expect("fail");
-    assert!(output.status.success());
-
-    let stdout = String::from_utf8(output.stdout).expect("invalid UTF-8");
-    assert_eq!(stdout, expected);
-    Ok(())
-}
-
 // --------------------------------------------------
 #[test]
 fn empty_empty() -> Result<()> {
@@ -144,70 +119,67 @@ fn empty_empty() -> Result<()> {
 // --------------------------------------------------
 #[test]
 fn file1_file1() -> Result<()> {
-    run(&[FILE1, FILE1], "tests/expected/file1_file1.out")
+    run!("tests/expected/file1_file1.out", FILE1, FILE1)
 }
 
 // --------------------------------------------------
 #[test]
 fn file1_file2() -> Result<()> {
-    run(&[FILE1, FILE2], "tests/expected/file1_file2.out")
+    run!("tests/expected/file1_file2.out", FILE1, FILE2)
 }
 
 // --------------------------------------------------
 #[test]
 fn file1_empty() -> Result<()> {
-    run(&[FILE1, EMPTY], "tests/expected/file1_empty.out")
+    run!("tests/expected/file1_empty.out", FILE1, EMPTY)
 }
 
 // --------------------------------------------------
 #[test]
 fn empty_file2() -> Result<()> {
-    run(&[EMPTY, FILE2], "tests/expected/empty_file2.out")
+    run!("tests/expected/empty_file2.out", EMPTY, FILE2)
 }
 
 // --------------------------------------------------
 #[test]
 fn file1_file2_1() -> Result<()> {
-    run(&["-1", FILE1, FILE2], "tests/expected/file1_file2.1.out")
+    run!("tests/expected/file1_file2.1.out", "-1", FILE1, FILE2)
 }
 
 // --------------------------------------------------
 #[test]
 fn file1_file2_2() -> Result<()> {
-    run(&["-2", FILE1, FILE2], "tests/expected/file1_file2.2.out")
+    run!("tests/expected/file1_file2.2.out", "-2", FILE1, FILE2)
 }
 
 // --------------------------------------------------
 #[test]
 fn file1_file2_3() -> Result<()> {
-    run(&["-3", FILE1, FILE2], "tests/expected/file1_file2.3.out")
+    run!("tests/expected/file1_file2.3.out", "-3", FILE1, FILE2)
 }
 
 // --------------------------------------------------
 #[test]
 fn file1_file2_1_2() -> Result<()> {
-    run(&["-12", FILE1, FILE2], "tests/expected/file1_file2.12.out")
+    run!("tests/expected/file1_file2.12.out", "-12", FILE1, FILE2)
 }
 
 // --------------------------------------------------
 #[test]
 fn file1_file2_2_3() -> Result<()> {
-    run(&["-23", FILE1, FILE2], "tests/expected/file1_file2.23.out")
+    run!("tests/expected/file1_file2.23.out", "-23", FILE1, FILE2)
 }
 
 // --------------------------------------------------
 #[test]
 fn file1_file2_13() -> Result<()> {
-    run(&["-13", FILE1, FILE2], "tests/expected/file1_file2.13.out")
+    run!("tests/expected/file1_file2.13.out", "-13", FILE1, FILE2)
 }
 
 // --------------------------------------------------
 #[test]
 fn file1_file2_123() -> Result<()> {
-    run(
-        &["-123", FILE1, FILE2],
-        "tests/expected/file1_file2.123.out",
-    )
+    run!("tests/expected/file1_file2.123.out", "-123", FILE1, FILE2)
 }
 
 // --------------------------------------------------
@@ -215,63 +187,84 @@ fn file1_file2_123() -> Result<()> {
 // --------------------------------------------------
 #[test]
 fn file1_file2_1_i() -> Result<()> {
-    run(
-        &["-1", "-i", FILE1, FILE2],
+    run!(
         "tests/expected/file1_file2.1.i.out",
+        "-1",
+        "-i",
+        FILE1,
+        FILE2
     )
 }
 
 // --------------------------------------------------
 #[test]
 fn file1_file2_2_i() -> Result<()> {
-    run(
-        &["-2", "-i", FILE1, FILE2],
+    run!(
         "tests/expected/file1_file2.2.i.out",
+        "-2",
+        "-i",
+        FILE1,
+        FILE2
     )
 }
 
 // --------------------------------------------------
 #[test]
 fn file1_file2_3_i() -> Result<()> {
-    run(
-        &["-3", "-i", FILE1, FILE2],
+    run!(
         "tests/expected/file1_file2.3.i.out",
+        "-3",
+        "-i",
+        FILE1,
+        FILE2
     )
 }
 
 // --------------------------------------------------
 #[test]
 fn file1_file2_1_2_i() -> Result<()> {
-    run(
-        &["-12", "-i", FILE1, FILE2],
+    run!(
         "tests/expected/file1_file2.12.i.out",
+        "-12",
+        "-i",
+        FILE1,
+        FILE2
     )
 }
 
 // --------------------------------------------------
 #[test]
 fn file1_file2_2_3_i() -> Result<()> {
-    run(
-        &["-23", "-i", FILE1, FILE2],
+    run!(
         "tests/expected/file1_file2.23.i.out",
+        "-23",
+        "-i",
+        FILE1,
+        FILE2
     )
 }
 
 // --------------------------------------------------
 #[test]
 fn file1_file2_13_i() -> Result<()> {
-    run(
-        &["-13", "-i", FILE1, FILE2],
+    run!(
         "tests/expected/file1_file2.13.i.out",
+        "-13",
+        "-i",
+        FILE1,
+        FILE2
     )
 }
 
 // --------------------------------------------------
 #[test]
 fn file1_file2_123_i() -> Result<()> {
-    run(
-        &["-123", "-i", FILE1, FILE2],
+    run!(
         "tests/expected/file1_file2.123.i.out",
+        "-123",
+        "-i",
+        FILE1,
+        FILE2
     )
 }
 
@@ -281,94 +274,131 @@ fn stdin_file1() -> Result<()> {
     run_stdin!(
         FILE1,
         "tests/expected/file1_file2.123.i.out",
-        "-123", "-i", "-", FILE2,
+        "-123",
+        "-i",
+        "-",
+        FILE2,
     )
 }
 
 // --------------------------------------------------
 #[test]
 fn stdin_file2() -> Result<()> {
-    run_stdin(
-        &["-123", "-i", FILE1, "-"],
+    run_stdin!(
         FILE2,
         "tests/expected/file1_file2.123.i.out",
+        "-123",
+        "-i",
+        FILE1,
+        "-",
     )
 }
 
 // --------------------------------------------------
 #[test]
 fn file1_file2_delim() -> Result<()> {
-    run(
-        &[FILE1, FILE2, "-d", ":"],
+    run!(
         "tests/expected/file1_file2.delim.out",
+        FILE1,
+        FILE2,
+        "-d",
+        ":"
     )
 }
 
 // --------------------------------------------------
 #[test]
 fn file1_file2_1_delim() -> Result<()> {
-    run(
-        &[FILE1, FILE2, "-1", "-d", ":"],
+    run!(
         "tests/expected/file1_file2.1.delim.out",
+        FILE1,
+        FILE2,
+        "-1",
+        "-d",
+        ":"
     )
 }
 
 // --------------------------------------------------
 #[test]
 fn file1_file2_2_delim() -> Result<()> {
-    run(
-        &[FILE1, FILE2, "-2", "-d", ":"],
+    run!(
         "tests/expected/file1_file2.2.delim.out",
+        FILE1,
+        FILE2,
+        "-2",
+        "-d",
+        ":"
     )
 }
 
 // --------------------------------------------------
 #[test]
 fn file1_file2_3_delim() -> Result<()> {
-    run(
-        &[FILE1, FILE2, "-3", "-d", ":"],
+    run!(
         "tests/expected/file1_file2.3.delim.out",
+        FILE1,
+        FILE2,
+        "-3",
+        "-d",
+        ":"
     )
 }
 
 // --------------------------------------------------
 #[test]
 fn file1_file2_12_delim() -> Result<()> {
-    run(
-        &[FILE1, FILE2, "-12", "-d", ":"],
+    run!(
         "tests/expected/file1_file2.12.delim.out",
+        FILE1,
+        FILE2,
+        "-12",
+        "-d",
+        ":"
     )
 }
 
 // --------------------------------------------------
 #[test]
 fn file1_file2_23_delim() -> Result<()> {
-    run(
-        &[FILE1, FILE2, "-23", "-d", ":"],
+    run!(
         "tests/expected/file1_file2.23.delim.out",
+        FILE1,
+        FILE2,
+        "-23",
+        "-d",
+        ":"
     )
 }
 
 // --------------------------------------------------
 #[test]
 fn file1_file2_13_delim() -> Result<()> {
-    run(
-        &[FILE1, FILE2, "-13", "-d", ":"],
+    run!(
         "tests/expected/file1_file2.13.delim.out",
+        FILE1,
+        FILE2,
+        "-13",
+        "-d",
+        ":"
     )
 }
 
 // --------------------------------------------------
 #[test]
 fn file1_file2_123_delim() -> Result<()> {
-    run(
-        &[FILE1, FILE2, "-123", "-d", ":"],
+    run!(
         "tests/expected/file1_file2.123.delim.out",
+        FILE1,
+        FILE2,
+        "-123",
+        "-d",
+        ":"
     )
 }
 
 // --------------------------------------------------
 #[test]
 fn blank_file1() -> Result<()> {
-    run(&[BLANK, FILE1], "tests/expected/blank_file1.out")
+    run!("tests/expected/blank_file1.out", BLANK, FILE1)
 }
