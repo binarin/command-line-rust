0dae5d8284823d6cda8cdf94111aebb8fa4682c6
Alexey Lebedeff <binarin@binarin.info>
2026-01-23 12:33:31 +0100

    Add filename parsing ('-' for stdin)/opening to the shared lib

--- learnr/src/lib.rs
+++ learnr/src/lib.rs
@@ -1,7 +1,50 @@
+use std::{fs::File, io::{BufRead, BufReader}};
+
+use anyhow::{Result, anyhow};
+
 #[derive(Debug, Clone, PartialEq)]
 pub enum CLIInput {
     StdIn,
     File(String),
+}
+
+pub fn open(filename: &CLIInput) -> Result<Box<dyn BufRead>> {
+    match filename {
+        CLIInput::StdIn => Ok(Box::new(BufReader::new(std::io::stdin()))),
+        CLIInput::File(path) => Ok(Box::new(BufReader::new(
+            File::open(&path).map_err(|err| anyhow!("{}: {err}", path))?,
+        )))
+    }
+}
+
+impl clap::builder::ValueParserFactory for CLIInput {
+    type Parser = CLIInputParser;
+
+    fn value_parser() -> Self::Parser {
+        CLIInputParser
+    }
+}
+
+#[derive(Clone)]
+pub struct CLIInputParser;
+
+impl clap::builder::TypedValueParser for CLIInputParser {
+    type Value = CLIInput;
+
+    fn parse_ref(
+        &self,
+        cmd: &clap::Command,
+        arg: Option<&clap::Arg>,
+        value: &std::ffi::OsStr,
+    ) -> Result<Self::Value, clap::Error> {
+        if value.eq("-") {
+            Ok(CLIInput::StdIn)
+        } else {
+            let string_parser = clap::builder::StringValueParser::new();
+            let val = string_parser.parse_ref(cmd, arg, value)?;
+            Ok(CLIInput::File(val))
+        }
+    }
 }
 
 #[cfg(test)]
