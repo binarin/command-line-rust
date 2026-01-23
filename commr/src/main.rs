7764d5baae90be44c01d4323adf32052483e534f
Alexey Lebedeff <binarin@binarin.info>
2026-01-23 16:24:57 +0100

    Chapter 10 - finalize `commr` implementation, all tests pass now

--- commr/src/main.rs
+++ commr/src/main.rs
@@ -1,6 +1,7 @@
 use anyhow::{Result, bail};
 use clap::Parser;
 use learnr::{CLIInput, open};
+use std::{cmp::Ordering, io::BufRead};
 
 /// ’comm’ in Rust
 #[derive(Debug, Parser)]
@@ -43,8 +44,65 @@ fn main() -> Result<()> {
     if args.file1 == CLIInput::StdIn && args.file2 == CLIInput::StdIn {
         bail!(r#"Both input files cannot be STDIN ("-")"#);
     }
-    let _fh1 = open(&args.file1)?;
-    let _fh2 = open(&args.file2)?;
-    dbg!(args);
+    let fh1 = open(&args.file1)?;
+    let fh2 = open(&args.file2)?;
+
+    let mut iter1 = fh1.lines();
+    let mut iter2 = fh2.lines();
+
+    let mut line1 = iter1.next().transpose()?;
+    let mut line2 = iter2.next().transpose()?;
+
+    let c2_prefix = if args.show_col1 {
+        args.delimiter.clone()
+    } else {
+        String::new()
+    };
+    let c3_prefix = if args.show_col2 {
+        c2_prefix.clone() + &args.delimiter
+    } else {
+        c2_prefix.clone()
+    };
+
+    loop {
+        let ord = match (&line1, &line2) {
+            (None, None) => break,
+            (Some(s1), Some(s2)) => {
+                if args.insensitive {
+                    s1.to_lowercase().cmp(&s2.to_lowercase())
+                } else {
+                    s1.cmp(&s2)
+                }
+            }
+
+            // EOF is always the biggest
+            (None, Some(_)) => Ordering::Greater,
+            (Some(_), None) => Ordering::Less,
+        };
+
+        // l1 ? l2
+        match ord {
+            Ordering::Less => {
+                if args.show_col1 {
+                    println!("{}", line1.unwrap());
+                }
+                line1 = iter1.next().transpose()?;
+            }
+            Ordering::Greater => {
+                if args.show_col2 {
+                    println!("{c2_prefix}{}", line2.unwrap());
+                }
+                line2 = iter2.next().transpose()?;
+            }
+            Ordering::Equal => {
+                if args.show_col3 {
+                    println!("{c3_prefix}{}", line1.unwrap());
+                }
+                line1 = iter1.next().transpose()?;
+                line2 = iter2.next().transpose()?;
+            }
+        }
+    }
+
     Ok(())
 }
