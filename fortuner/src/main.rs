6da40f2b5096fec3f8cfac604a3ace35e20f27c0
Alexey Lebedeff <binarin@binarin.info>
2026-01-28 14:58:56 +0100

    Chapter 12 - implement args parsing

--- fortuner/src/main.rs
+++ fortuner/src/main.rs
@@ -1,14 +1,55 @@
+use anyhow::Result;
 use clap::Parser;
+use regex::{Regex, RegexBuilder};
 
+/// Rust version of ‘fortune’
 #[derive(Debug, Parser)]
-struct Args {
+#[command[about, author, version]]
+struct CLIArgs {
+    /// Input files or directories
+    #[arg(value_name = "FILE", required = true)]
     sources: Vec<String>,
+
+    /// Pattern
+    #[arg(short = 'm', long)]
     pattern: Option<String>,
+
+    /// Case-insensitive pattern matching
+    #[arg(short, long)]
     insensitive: bool,
+
+    /// Random seed
+    #[arg(short, long)]
     seed: Option<u64>,
 }
 
+#[derive(Debug)]
+struct Args {
+    sources: Vec<String>,
+    pattern: Option<Regex>,
+}
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<()> {
+    let args = parse_args()?;
+    dbg!(args);
+    Ok(())
+}
+
+fn parse_args() -> Result<Args> {
+    let CLIArgs {
+        sources,
+        pattern,
+        insensitive,
+        ..
+    } = CLIArgs::parse();
+
+    let pattern = pattern
+        .map(|pat| {
+            RegexBuilder::new(pat.as_str())
+                .case_insensitive(insensitive)
+                .build()
+        })
+        .transpose()?;
+
+    Ok(Args { sources, pattern })
 }
