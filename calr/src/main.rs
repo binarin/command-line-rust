d8e3c759b179293b7f84ebc61b83baea5512dff8
Alexey Lebedeff <binarin@binarin.info>
2026-01-31 12:17:31 +0100

    Chapter 13 - implement function to format a single month

--- calr/src/main.rs
+++ calr/src/main.rs
@@ -1,5 +1,7 @@
 use anyhow::{Result, anyhow, bail};
+use chrono::{Datelike, NaiveDate, TimeZone, Weekday};
 use clap::Parser;
+use itertools::Itertools;
 
 /// Rust version of ‘cal’
 #[derive(Debug, Parser)]
@@ -11,7 +13,7 @@ struct CLIArgs {
 
     /// Month name or number (1-12)
     #[arg(short, value_parser = month_arg_parser)]
-    month: Option<String>,
+    month: Option<u32>,
 
     /// Show whole current year
     #[arg(
@@ -23,25 +25,98 @@ struct CLIArgs {
     show_current_year: bool,
 }
 
+#[derive(Debug)]
+enum Period {
+    Month(i32, u32),
+    Year(i32),
+}
+
+#[derive(Debug)]
+struct Args {
+    period: Period,
+}
+
 fn main() -> Result<()> {
-    let args = CLIArgs::parse();
+    let args = parse_args(&CLIArgs::parse())?;
     dbg!(args);
     Ok(())
 }
 
+fn parse_args(cli_args: &CLIArgs) -> Result<Args> {
+    let now = chrono::Local::now();
+    let period = match (cli_args.year, cli_args.month, cli_args.show_current_year) {
+        (_, _, true) => Period::Year(now.year()),
+        (None, None, _) => Period::Month(now.year(), now.month()),
+        (Some(year), None, _) => Period::Year(year),
+        (None, Some(month), false) => Period::Month(now.year(), month),
+        (Some(year), Some(month), false) => Period::Month(year, month),
+    };
+
+    Ok(Args { period })
+}
+
+const BLOCK_WIDTH: usize = 2 /* sun */ + 3 * 6 /* mon-sat */;
+const HORIZONTAL_SEPARATOR: &str = "  ";
+
+fn format_month(year: i32, month: u32, print_year: bool, today: NaiveDate) -> Vec<String> {
+    let mut label: String = MONTH_NAMES[month as usize - 1].to_string();
+    if print_year {
+        label += &format!(" {year}").to_string();
+    }
+    let mut rows = vec![
+        format!("{label:^width$}", width = BLOCK_WIDTH,),
+        "Su Mo Tu We Th Fr Sa".to_string(),
+    ];
+
+    let today_day: u32 = if year == today.year() && month == today.month() {
+        today.day()
+    } else {
+        std::u32::MAX
+    };
+
+    let dt = chrono::NaiveDate::from_ymd_opt(year, month, 1).unwrap();
+
+    let mut days: Vec<String> = vec![];
+
+    let filler_num = dt.weekday().number_from_sunday() - 1;
+    (1..=filler_num).for_each(|_| days.push("  ".to_string()));
+    (1..=dt.num_days_in_month()).for_each(|day| {
+        let mut rendered = format!("{day:>2}");
+        if today_day == day.into() {
+            rendered = ansi_term::Style::new()
+                .reverse()
+                .paint(rendered)
+                .to_string();
+        }
+        days.push(rendered);
+    });
+    (days.len()..42).for_each(|_| days.push("  ".to_string()));
+
+    rows.extend(
+        days.into_iter()
+            .chunks(7)
+            .into_iter()
+            .map(|ds| itertools::join(ds, " ")),
+    );
+
+    rows.iter_mut()
+        .for_each(|r: &mut String| *r += HORIZONTAL_SEPARATOR);
+    rows
+}
+
 const MONTH_NAMES: [&str; 12] = [
-    "january",
-    "february",
-    "march",
-    "april",
-    "may",
-    "june",
-    "july",
-    "august",
-    "septemper",
-    "october",
-    "november",
-    "december",
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "Septemper",
+    "October",
+    "November",
+    "December",
 ];
 
 fn month_arg_parser(arg: &str) -> Result<u32> {
@@ -53,13 +128,15 @@ fn month_arg_parser(arg: &str) -> Result<u32> {
         return Err(anyhow!(r#"month "{arg}" not in the range 1 through 12"#));
     }
 
-    let candidates: Vec<(u32, &str)> = (1..=12)
-        .zip(MONTH_NAMES)
-        .filter(|(_, n)| n.starts_with(arg))
+    let candidates: Vec<(String, u32)> = MONTH_NAMES
+        .into_iter()
+        .map(str::to_lowercase)
+        .zip(1..=12)
+        .filter(|(n, _)| n.starts_with(arg))
         .collect();
 
     match candidates.as_slice() {
-        [(idx, _)] => return Ok(*idx),
+        [(_, idx)] => return Ok(*idx),
         [_, ..] => bail!(r#"Ambigous month name "{arg}""#),
         [] => bail!(r#"Invalid month "{arg}""#),
     }
@@ -69,6 +146,7 @@ fn month_arg_parser(arg: &str) -> Result<u32> {
 mod tests {
     use assertables::*;
     use learnr::assert_err_str_contains;
+    use pretty_assertions::assert_eq;
 
     use super::*;
 
@@ -93,5 +171,46 @@ mod tests {
         assert_err_str_contains!(res, r#"Invalid month "foo""#);
 
         assert_err_str_contains!(month_arg_parser("ju"), "Ambigous");
+    }
+
+    #[test]
+    fn test_format_month() {
+        let today = NaiveDate::from_ymd_opt(0, 1, 1).unwrap();
+        let leap_february = vec![
+            "   February 2020      ",
+            "Su Mo Tu We Th Fr Sa  ",
+            "                   1  ",
+            " 2  3  4  5  6  7  8  ",
+            " 9 10 11 12 13 14 15  ",
+            "16 17 18 19 20 21 22  ",
+            "23 24 25 26 27 28 29  ",
+            "                      ",
+        ];
+        assert_eq!(format_month(2020, 2, true, today), leap_february);
+
+        let may = vec![
+            "        May           ",
+            "Su Mo Tu We Th Fr Sa  ",
+            "                1  2  ",
+            " 3  4  5  6  7  8  9  ",
+            "10 11 12 13 14 15 16  ",
+            "17 18 19 20 21 22 23  ",
+            "24 25 26 27 28 29 30  ",
+            "31                    ",
+        ];
+        assert_eq!(format_month(2020, 5, false, today), may);
+
+        let april_hl = vec![
+            "     April 2021       ",
+            "Su Mo Tu We Th Fr Sa  ",
+            "             1  2  3  ",
+            " 4  5  6 \u{1b}[7m 7\u{1b}[0m  8  9 10  ",
+            "11 12 13 14 15 16 17  ",
+            "18 19 20 21 22 23 24  ",
+            "25 26 27 28 29 30     ",
+            "                      ",
+        ];
+        let today = NaiveDate::from_ymd_opt(2021, 4, 7).unwrap();
+        assert_eq!(format_month(2021, 4, true, today), april_hl);
     }
 }
