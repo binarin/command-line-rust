67535ec676ed4a8b635ab795bedc3d52fc7e5a99
Alexey Lebedeff <binarin@binarin.info>
2026-02-01 13:40:35 +0100

    Chapter 14 - implement arguments parsing

--- lsr/src/main.rs
+++ lsr/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::PathBuf;
+
+use anyhow::Result;
+use clap::Parser;
+
+/// Rust version of ’ls’
+#[derive(Debug, Parser)]
+#[command(author, about, version)]
+struct CLIArgs {
+    #[arg(value_name = "PATH", default_value = ".")]
+    paths: Vec<PathBuf>,
+
+    /// Long listing
+    #[arg(short, long)]
+    long: bool,
+
+    /// Show all files
+    #[arg(short = 'a', long = "all")]
+    show_hidden: bool,
+}
+
+fn main() -> Result<()> {
+    let args = CLIArgs::parse();
+    dbg!(args);
+    Ok(())
 }
